ac5ab0c0eac359efc36f46064ccd26f6378cbfd6
MuhammadHatta72 <attah1335@gmail.com>
2023-05-13 21:50:27 +0700

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="flex justify-center items-center w-screen h-screen">
+        <h1 class="text-3xl font-medium">
+            LARAVEL TAILWIND
+        </h1>
+    </div>
+
+    @vite('resources/js/app.js')
+</body>
+
+</html>
